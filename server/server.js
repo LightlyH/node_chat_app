2d71337892bc49475055dd17df25dc9cb2431af8
Lily Hu <hu.yit@husky.neu.edu>
2018-07-01 06:25:10 -0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -13,11 +13,17 @@ var io = socketIO(server);
 io.on('connection', socket => {
   console.log('New user connected');
 
-  // socket.emit('newMessage', {
-  //   from: 'Sasha',
-  //   text: 'Good morning!',
-  //   createdAt: 123
-  // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', message => { // in back-end, arrow funcs are allowed
     console.log('createMessage', message);
@@ -26,6 +32,11 @@ io.on('connection', socket => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
